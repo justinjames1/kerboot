71b03623683f7f9098e8aae70ddc2b8af0417e59
Justin <justindude057@gmail.com>
2026-06-04 00:13:44 -0400

    Update bios.asm

--- kerboot/bios.asm
+++ kerboot/bios.asm
@@ -1,3 +1,4 @@
+bits 16
 title db"KERBOOT [BIOS]"
 load db"LOADING KERNEL."
     mov si, title     
@@ -34,11 +35,35 @@ load:
 hlt: 
 hlt
 
-;someone write the code to load kernel to ram
+disk_data: resb 512
 
-
+mov ah, 2
+mov ch, 1
+mov cl, 2
+mov dh, 1
+mov ah, 42h ;read from disk
+mov dl, 0x80 ;specify we are reading from the C: drive
 int 0x13 ;disk controller interrupt
+
+    mov si, disk_data   
+    mov ah, 0x0e       
+
+mov al, 0x0D
+int 0x10
+mov al, 0x0A
+int 0x10
+
+.diskprint_loop:        
+    lodsb         
+    or al, al       
+    jz .hlt       
+    int 0x10        
+    jmp .diskprint_loop
+
+.hlt: 
+hlt
+
+
+
 time 510 db 0
 db 0x55, 0xaa 
-hlt: 
-hlt
